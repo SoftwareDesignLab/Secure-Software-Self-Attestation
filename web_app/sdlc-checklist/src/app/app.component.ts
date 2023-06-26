84bc7138d1ce610fca8292bf450ce33151a167aa
Patrick Elser <pte6144@rit.edu>
2023-06-26 11:01:11 -0400

    Added arrows and collapse all buttons, fixed an error where collapsing a group erased selections

--- web_app/sdlc-checklist/src/app/app.component.ts
+++ web_app/sdlc-checklist/src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ViewChildren, QueryList } from '@angular/core';
+import { GroupComponent } from './group/group.component';
 
 @Component({
   selector: 'app-root',
@@ -7,8 +8,19 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   catalogData: any;
+  showComponentsArray: any;
+  @ViewChildren(GroupComponent) childComponents!: QueryList<GroupComponent>;
   
   onFileSelected(jsonData: any): void {
     this.catalogData = jsonData;
+  }
+
+  setAllGroupExpansion(toSet: boolean): void {
+    for(let i = this.childComponents.length - 1; i >= 0; i--) {
+      let child = this.childComponents.get(i);
+      if (child instanceof GroupComponent) {
+        child.showComponents = toSet;
+      }
+    }
   }
 }
