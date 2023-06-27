fba4a2d9b147653d801ea9abb5ab0cfb0f78005e
Patrick Elser <pte6144@rit.edu>
2023-06-27 17:31:36 -0400

    Added buttons to select and deselect all boxes within a group

--- web_app/sdlc-checklist/src/app/group/group.component.ts
+++ web_app/sdlc-checklist/src/app/group/group.component.ts
@@ -20,15 +20,44 @@ export class GroupComponent {
       this.hideChildRollable();
     }
   }
+
   setComponents(truth: boolean) {
     if (!truth) {
       this.hideChildRollable();
     }
     this.showComponents = truth;
   }
+
   hideChildRollable() {
     this.childComponents.forEach((child) => {
       child.showRollable = false;
+    })
+  }
+
+  areAllChecked(): boolean {
+    if (this.childComponents === undefined) {
+      return false;
+    }
+    for (let i = this.childComponents.length - 1; i >= 0; i--) {
+      let child = this.childComponents.get(i);
+      if (child instanceof ChecklistItemComponent) {
+        let box = document.getElementById('checkbox-'+ child.id);
+        if (box instanceof HTMLInputElement) {
+          if (!box.checked) {
+            return false;
+          }
+        }
+      }
+    }
+    return true;
+  }
+
+  setAllChildren(truth: boolean) {
+    this.childComponents.forEach((child) => {
+      let box = document.getElementById('checkbox-' + child.id);
+      if(box instanceof HTMLInputElement) {
+        box.checked = truth;
+      }
     })
   }
 }
