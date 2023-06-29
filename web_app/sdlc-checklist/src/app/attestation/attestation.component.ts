46c4d75e3a7861405c3ce5efdf9d80858f4a6214
ehn4602 <ehn4602@g.rit.edu>
2023-06-29 16:55:35 -0400

    added comment class to store values for form

--- web_app/sdlc-checklist/src/app/attestation/attestation.component.ts
+++ web_app/sdlc-checklist/src/app/attestation/attestation.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { MatDialog } from '@angular/material/dialog';
 import { MatDialogRef } from '@angular/material/dialog';
+import { attestationComment } from '../attestationForm';
 
 
 @Component({
@@ -11,6 +12,8 @@ import { MatDialogRef } from '@angular/material/dialog';
 })
 export class AttestationComponent {
   selectedValue: string = 'company'; 
+  row: number = 1;
+  info: Array<attestationComment> = new Array<attestationComment>;
 
   constructor(private dialogRef: MatDialogRef<AttestationComponent>){}
 
@@ -18,5 +21,44 @@ export class AttestationComponent {
   onSubmit() {
     console.log(this.selectedValue); 
     this.dialogRef.close();
+  }
+  addRow(){
+    this.info.push(new attestationComment);
+    console.log(this.info.length);
+  }
+  removeRow(){
+    this.info.pop();
+    console.log(this.info.length);
+  }
+
+  ngOnInit(){
+    this.info.push(new attestationComment);
+  }
+
+  onKey(event: any, attest: attestationComment, target: string) { // without type info
+    if(target==="name") {
+      attest.addName(event.target.value);
+    } else if (target==="version") {
+      attest.addVersion(event.target.value)
+    } else if (target==="date") {
+      attest.addDate(event.target.value)
+    }
+  }
+
+  validComments(){
+    let valid = true;
+    this.info.forEach(function (comment) {  
+      if(!comment.isFilled()){
+        valid=false;
+      }  
+      });  
+      return valid;
+    }
+
+  debug(){
+    console.log(this.info[0].getName());
+    console.log(this.info[0].getVersion());
+    console.log(this.info[0].getDate());
+    console.warn(this.info[0].isFilled());
   }
 }
