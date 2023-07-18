4e77dbd8591f8f979c2a213aa6675a18015e882b
Joseph <jcvita2012@gmail.com>
2023-07-18 03:30:15 -0400

    assessment plan model data layout

--- web_app/sdlc-checklist/src/app/models/assessmentPlan.ts
+++ web_app/sdlc-checklist/src/app/models/assessmentPlan.ts
@@ -1,12 +1,57 @@
 import { uuid } from 'uuidv4';
 
+class AssessmentPart {
+  name: String = "";
+  title?: String;
+  uuid?: String;
+  ns?: String;
+  class?: String;
+  props?: Prop[];
+  prose?: String;
+  parts?: AssessmentPart[];
+  links?: Link[];
+
+  generateUUID() {
+    this.uuid = uuid();
+  }
+}
+
+class TermsAndConditions {
+  parts: AssessmentPart[] = [];
+
+  addPart(name: String, title: String, uuid: String, ns: String, class_: String, props: Prop[], prose: String, parts: AssessmentPart[], links: Link[]) {
+    let newPart = new AssessmentPart();
+    newPart.name = name;
+    newPart.title = title;
+    newPart.uuid = uuid;
+    newPart.ns = ns;
+    newPart.class = class_;
+    newPart.props = props;
+    newPart.prose = prose;
+    newPart.parts = parts;
+    newPart.links = links;
+    this.parts.push(newPart);
+  }
+}
+
 class ImportSSP {
   href: String = "";
   remarks?: String;
 }
 
-class ControlSelection {
+class ControlID {
+  "control-id": String = "";
+  "statement-ids"?: String[];
+}
 
+class ControlSelection {
+  description?: String;
+  props?: Prop[];
+  links?: Link[];
+  "include-all"?: Boolean;
+  "include-controls"?: ControlID[];
+  "exclude-controls"?: ControlID[];
+  remarks?: String;
 }
 
 class Link {
@@ -46,6 +91,8 @@ class Party {
   addresses?: Address[];
   props?: Prop[];
   remarks?: String;
+  private _first_name?: String;
+  private _last_name?: String;
 
   addAddress(addrlines: String[], city: String, state: String, postal: String, country: String) {
     let newAddress = new Address();
@@ -89,6 +136,25 @@ class Party {
   }
 }
 
+class SubjectID {
+  type: String = "component"; //can be component, inventory-item, location, party, user or resource
+  "subject-uuid": String = "";
+  props?: Prop[];
+  links?: Link[];
+  remarks?: String;
+}
+
+class AssessmentSubject {
+  type: String = "party"; //can be component, inventory-item, location, party or user
+  description?: String;
+  props?: Prop[];
+  links?: Link[];
+  remarks?: String;
+  "include-all"?: Boolean;
+  "include-subjects"?: SubjectID[];
+  "exclude-subjects"?: SubjectID[];
+}
+
 class APMetadata {
   title: String = "Assessment Plan";
   "last-modified": String = new Date().toISOString();
@@ -111,9 +177,22 @@ class APMetadata {
   }
 }
 
+//TODO local definitions
+// class LocalDefinitions {
+
+// }
+
+//TODO assessment assets
+// class AssessmentAssets {
+
+// }
+
 export class AssessmentPlan {
   uuid: String = uuid();
   metadata: APMetadata = new APMetadata();
-
+  "import-ssp": ImportSSP = new ImportSSP();
+  "reviewed-controls": ReviewedControls = new ReviewedControls();
+  "assessment-subjects": AssessmentSubject[] = [];
+  "terms-and-conditions": TermsAndConditions = new TermsAndConditions();
 }
   
