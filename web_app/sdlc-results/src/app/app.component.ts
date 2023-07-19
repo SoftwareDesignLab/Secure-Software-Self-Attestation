9c5556a09110cf43ccc0cb64f4f046a0028fb81e
Patrick Elser <pte6144@rit.edu>
2023-07-19 10:31:14 -0400

    Added a blank nav

--- web_app/sdlc-results/src/app/app.component.ts
+++ web_app/sdlc-results/src/app/app.component.ts
@@ -33,6 +33,7 @@ import { AssessmentResults } from './resultsModel';
 export class AppComponent {
   assessmentResults: AssessmentResults | undefined;
   showFullFooter: boolean = false;
+  showNav = false;
 
   onFileSelected(jsonData: any): void { //TODO jsonData should be of type Catalog
     this.assessmentResults = jsonData["assessment-results"];
@@ -40,5 +41,19 @@ export class AppComponent {
 
   toggleFooter() {
     this.showFullFooter = !this.showFullFooter;
+  }
+
+  toggleNav(): void {
+    this.showNav = !this.showNav;
+    let nav = document.getElementById('nav');
+    if (nav instanceof HTMLElement) {
+      if (this.showNav) {
+        nav.classList.add('nav-opening');
+        nav.classList.remove('nav-closing');
+      } else {
+        nav.classList.add('nav-closing');
+        nav.classList.remove('nav-opening');
+      }
+    }
   }
 }
