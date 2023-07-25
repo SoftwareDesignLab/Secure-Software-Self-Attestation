daa9a36427da46edcb1f0bb90a0bd1d07ffa5a5b
Joseph <jcvita2012@gmail.com>
2023-07-25 05:15:26 -0400

    serialization

--- web_app/sdlc-checklist/src/app/models/assessmentPlan.ts
+++ web_app/sdlc-checklist/src/app/models/assessmentPlan.ts
@@ -31,6 +31,21 @@ class AssessmentPart {
   generateUUID() {
     this.uuid = uuid();
   }
+
+  serialize(): object {
+    let serialized = {
+      "name": this.name,
+      "title": this.title,
+      "uuid": this.uuid,
+      "ns": this.ns,
+      "class": this.class,
+      "props": this.props?.map(prop => prop.serialize()),
+      "prose": this.prose,
+      "parts": this.parts?.map(part => part.serialize()),
+      "links": this.links?.map(link => link.serialize()),
+    };
+    return serialized;
+  }
 }
 
 class TermsAndConditions {
@@ -49,23 +64,47 @@ class TermsAndConditions {
     if (links !== undefined ) newPart.links = links;
     this.parts.push(newPart);
   }
+
+  serialize(): object {
+    let serialized = [];
+    for (let part of this.parts) {
+      serialized.push(part.serialize());
+    }
+    return serialized;
+  }
 }
 
 class ImportSSP {
   href: String = "";
   remarks?: String;
+
+  serialize(): object {
+    let serialized = {
+      "href": this.href,
+      "remarks": this.remarks,
+    };
+    return serialized;
+  }
 }
 
 class ControlID {
   "control-id": String = "";
   "statement-ids"?: String[];
+
+  serialize(): object {
+    let serialized = {
+      "control-id": this["control-id"],
+      "statement-ids": this["statement-ids"],
+    };
+    return serialized;
+  }
 }
 
 export class ControlSelection {
   description?: String;
   props?: Prop[];
   links?: Link[];
-  "include-all"?: Boolean;
+  "include-all"?: Boolean | object;
   "include-controls"?: ControlID[];
   "exclude-controls"?: ControlID[];
   remarks?: String;
@@ -82,9 +121,7 @@ export class ControlSelection {
 
   addProp(name: String, value: String, class_?: String, uuid?: String, ns?: String, remarks?: String) {
     if (this.props === undefined) this.props = [];
-    let newProp = new Prop();
-    newProp.name = name;
-    newProp.value = value;
+    let newProp = new Prop(name, value);
     if (class_ !== undefined) newProp.class = class_;
     if (uuid !== undefined) newProp.uuid = uuid;
     if (ns !== undefined) newProp.ns = ns;
@@ -139,11 +176,37 @@ export class ControlSelection {
     let index = this.props.findIndex(prop => prop.name === name && prop.class === class_);
     if (index > -1) this.props.splice(index, 1);
   }
+
+  serialize(): object {
+    let serialized = {
+      "description": this.description,
+      "props": this.props?.map(prop => prop.serialize()),
+      "links": this.links?.map(link => link.serialize()),
+      "include-all": this["include-all"],
+      "include-controls": this["include-controls"]?.map(control => control.serialize()),
+      "exclude-controls": this["exclude-controls"]?.map(control => control.serialize()),
+      "remarks": this.remarks,
+    };
+    if (serialized['include-all']) {
+      delete serialized['include-controls'];
+      delete serialized['exclude-controls'];
+      serialized['include-all'] = {};
+    }
+    return serialized;
+  }
 }
 
 class Link {
   href: String = "";
   rel?: String;
+
+  serialize(): object {
+    let serialized = {
+      "href": this.href,
+      "rel": this.rel,
+    };
+    return serialized;
+  }
 }
 
 class ReviewedControls {
@@ -154,7 +217,6 @@ class ReviewedControls {
   remarks?: String;
 
   constructor() {
-    this.addControlSelection();
   }
 
   addControlSelection(description: String = "", props: Prop[] = [], links: Link[] = [], includeAll: Boolean = false, includeControls: ControlID[] = [], excludeControls: ControlID[] = [], remarks: String = "") {
@@ -164,24 +226,59 @@ class ReviewedControls {
 
   addProp(name: String, value: String, class_?: String, uuid?: String, ns?: String, remarks?: String) {
     if (this.props === undefined) this.props = [];
-    let newProp = new Prop();
-    newProp.name = name;
-    newProp.value = value;
+    let newProp = new Prop(name, value);
     newProp.class = class_;
     newProp.uuid = uuid;
     newProp.ns = ns;
     newProp.remarks = remarks;
     this.props.push(newProp);
   }
+
+  //For serialization, if controlSelection is empty, add a blank controlSelection
+  serialize(): object {
+    let serialized = {
+      "control-selections": this["control-selections"].map(controlSelection => controlSelection.serialize()),
+      "props": this.props?.map(prop => prop.serialize()),
+      "links": this.links?.map(link => link.serialize()),
+      "description": this.description,
+      "remarks": this.remarks,
+    };
+    if (serialized['control-selections'].length === 0) {
+      let newControlSelection = new ControlSelection();
+      serialized['control-selections'].push(newControlSelection.serialize());
+    }
+    return serialized;
+  }
 } 
 
-class Prop {
-  name: String = "";
-  value: String = "";
+export class Prop {
+  name: String;
+  value: String;
   class?: String;
   uuid?: String;
   ns?: String;
   remarks?: String;
+
+  constructor(name: String, value: String, class_?: String, uuid?: String, ns?: String, remarks?: String) {
+    this.name = name;
+    this.value = value;
+    this.class = class_;
+    this.uuid = uuid;
+    this.ns = ns;
+    this.remarks = remarks;
+  }
+
+  serialize(): object {
+    let serialized = {
+      "name": this.name,
+      "value": this.value,
+      "class": this.class,
+      "uuid": this.uuid,
+      "ns": this.ns,
+      "remarks": this.remarks,
+    };
+    return serialized;
+  }
 }
 
 class Address {
@@ -190,6 +287,17 @@ class Address {
   state: String = "";
   "postal-code": String = "";
   country: String = "";
+
+  serialize(): object {
+    let serialized = {
+      "addr-lines": this["addr-lines"],
+      "city": this.city,
+      "state": this.state,
+      "postal-code": this["postal-code"],
+      "country": this.country,
+    };
+    return serialized;
+  }
 }
 
 class Party {
@@ -264,14 +372,27 @@ class Party {
 
   addProp(name: String, value: String, class_?: String, uuid?: String, ns?: String, remarks?: String) {
     if (this.props === undefined) this.props = [];
-    let newProp = new Prop();
-    newProp.name = name;
-    newProp.value = value;
+    let newProp = new Prop(name, value);
     newProp.class = class_;
     newProp.uuid = uuid;
     newProp.ns = ns;
     newProp.remarks = remarks;
     this.props.push(newProp);
+  }
+
+  serialize(): object {
+    let serialized = {
+      "uuid": this.uuid,
+      "type": this.type,
+      "name": this.name,
+      "addresses": this.addresses?.map((address) => address.serialize()),
+      "props": this.props?.map((prop) => prop.serialize()),
+      "remarks": this.remarks,
+      "email-addresses": this["email-addresses"],
+      "telephone-numbers": this["telephone-numbers"],
+      "links": this.links?.map((link) => link.serialize()),
+    };
+    return serialized;
   }
 }
 
@@ -284,9 +405,7 @@ export class SubjectID {
 
   addProp(name: String, value: String, class_?: String, uuid?: String, ns?: String, remarks?: String) {
     if (this.props === undefined) this.props = [];
-    let newProp = new Prop();
-    newProp.name = name;
-    newProp.value = value;
+    let newProp = new Prop(name, value);
     newProp.class = class_;
     newProp.uuid = uuid;
     newProp.ns = ns;
@@ -305,6 +424,17 @@ export class SubjectID {
   setType(type: SubjectIDType) {
     this.type = type;
   }
+
+  serialize(): object {
+    let serialized = {
+      "type": this.type,
+      "subject-uuid": this["subject-uuid"],
+      "props": this.props?.map((prop) => prop.serialize()),
+      "remarks": this.remarks,
+      "links": this.links?.map((link) => link.serialize()),
+    };
+    return serialized;
+  }
 }
 
 export class AssessmentSubject {
@@ -313,7 +443,7 @@ export class AssessmentSubject {
   props?: Prop[];
   links?: Link[];
   remarks?: String;
-  "include-all"?: Boolean;
+  "include-all"?: Boolean | object;
   "include-subjects"?: SubjectID[];
   "exclude-subjects"?: SubjectID[];
   private subjectMemory: Array<Array<SubjectID> | undefined> = [[], []];
@@ -331,19 +461,29 @@ export class AssessmentSubject {
     }
   }
 
-  addSubjectID(type: SubjectIDType) {
+  includeNewSubject(type: SubjectIDType) {
     if (this["include-subjects"] === undefined) this["include-subjects"] = [];
     let newSubjectID = new SubjectID();
     newSubjectID.type = type;
     newSubjectID["subject-uuid"] = uuid();
-
+    this["include-subjects"].push(newSubjectID);
+    this.subjectMemory[0] = this["include-subjects"];
   }
+
+  excludeNewSubject(type: SubjectIDType) {
+    if (this["exclude-subjects"] === undefined) this["exclude-subjects"] = [];
+    let newSubjectID = new SubjectID();
+    newSubjectID.type = type;
+    newSubjectID["subject-uuid"] = uuid();
+    this["exclude-subjects"].push(newSubjectID);
+    this.subjectMemory[1] = this["exclude-subjects"];
+  }
+
+  //TODO move subject from exclude -> include or back
 
   addProp(name: String, value: String, class_?: String, uuid?: String, ns?: String, remarks?: String) {
     if (this.props === undefined) this.props = [];
-    let newProp = new Prop();
-    newProp.name = name;
-    newProp.value = value;
+    let newProp = new Prop(name, value);
     newProp.class = class_;
     newProp.uuid = uuid;
     newProp.ns = ns;
@@ -352,7 +492,24 @@ export class AssessmentSubject {
   }
 
   //TODO during serialization, if include-all is true, convert include-all to an empty object or undefined if it is false
-
+  serialize(): object {
+    let serialized = {
+      "type": this.type,
+      "description": this.description,
+      "props": this.props?.map(prop => prop.serialize()),
+      "remarks": this.remarks,
+      "include-all": this["include-all"],
+      "include-subjects": this["include-subjects"]?.map(subject => subject.serialize()),
+      "exclude-subjects": this["exclude-subjects"]?.map(subject => subject.serialize()),
+      "links": this.links?.map(link => link.serialize()),
+    };
+    if (this["include-all"] === true){
+      delete serialized["include-subjects"];
+      delete serialized["exclude-subjects"];
+      serialized["include-all"] = {};
+    };
+    return serialized;
+  }
 }
 
 class APMetadata {
@@ -379,6 +536,18 @@ class APMetadata {
 
   publish() {
     this.published = new Date().toISOString();
+  }
+
+  serialize(): object {
+    let serialized = {
+      "title": this.title,
+      "last-modified": this["last-modified"],
+      "version": this.version,
+      "oscal-version": this["oscal-version"],
+      "published": this.published,
+      "parties": this.parties.map(party => party.serialize()),
+    };
+    return serialized;
   }
 }
 
@@ -408,5 +577,17 @@ export class AssessmentPlan {
     this["assessment-subjects"].push(newAssessmentSubject);
   }
 
+
+  serialize(): object {
+    let serialized = {
+      "uuid": this.uuid,
+      "metadata": this.metadata.serialize(),
+      "import-ssp": this["import-ssp"].serialize(),
+      "reviewed-controls": this["reviewed-controls"].serialize(),
+      "assessment-subjects": this["assessment-subjects"]?.map((subject) => subject.serialize()),
+      "terms-and-conditions": this["terms-and-conditions"]?.serialize(),
+    };
+    return serialized;
+  }
 }
   
